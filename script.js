491248ac9fd28b8ce834a6cd05db8989f9cf274b
gitSuki <collinbrown226@gmail.com>
2022-03-07 23:27:34 -0600

    add test objects

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let myLibrary = []
 
-function Novel(name, author, year, language) {
+const display = document.getElementById('test')
+
+function Novel(name, author, language, year) {
+    //Assigning all the given parameter values to the object
     this.name = name
     this.author = author
-    this.year = year
     this.language = language
+    this.year = year
     this.read = false
 }
 
@@ -15,3 +18,13 @@ Novel.prototype.toggleRead = function() {
 Novel.prototype.addToLibrary = function() {
     myLibrary.push(this.name)
 }
+
+let gameOfThrones = new Novel ("A Game of Thrones", "George R.R. Martin", "English", 1996)
+let fateStayNight = new Novel ("Fate/Stay Night", "Kinoko Nasu", "Japanese", 2004)
+let crimeAndPunishment = new Novel("Crime and Punishment", "Fyodor Dostaevsky", "Russian", 1866)
+
+gameOfThrones.addToLibrary()
+fateStayNight.addToLibrary()
+crimeAndPunishment.addToLibrary()
+
+display.textContent = gameOfThrones.name
